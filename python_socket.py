20a3cfb823cd0edcfe1253b8da6cddc9ddea1157
Matej Ajster <matej.ajster@global.id>
2020-09-07 10:25:42 +0200

    clear python script

--- python_socket.py
+++ python_socket.py
@@ -55,25 +55,15 @@ def validateGenerateData(options, sio, namespace):
   return components, period, amplitudes, replicates, noise
 
 def generate_data(payload, sio, namespace):
-  sio.emit('print', 'generate_data', namespace=namespace)
 
   components, period, amplitudes, replicates,noise = validateGenerateData(payload, sio, namespace)
 
-  sio.emit('print', json.dumps(components), namespace=namespace)
-  sio.emit('print', json.dumps(period), namespace=namespace)
-  sio.emit('print', json.dumps(amplitudes), namespace=namespace)
-  sio.emit('print', json.dumps(noise), namespace=namespace)
-
   df = file_parser.generate_test_data(phase = 0, period = period, n_components = components, name = "test1", noise = noise, replicates = replicates, amplitudes = amplitudes, independent=False)
-
-  sio.emit('print', 'after file parser', namespace=namespace)
 
   csv_buffer = io.StringIO()
   file_parser.export_csv(df, csv_buffer)
 
   csv_string = csv_buffer.getvalue()
-
-  sio.emit('print', csv_string, namespace=namespace)
 
   return csv_string
 
@@ -104,10 +94,6 @@ def periodogram(payload, sio, namespace):
 
   df = getDataFrame(fileType, data, hasXlsxReplicates)
 
-  sio.emit('print', 'periodogram', namespace=namespace)
-  
-  sio.emit('print', json.dumps(options), namespace=namespace)
-
   figure_image_list = []
   cosinor.periodogram_df(df, per_type=options['per_type'], logscale=options['logscale'], max_per=options['max_per'], min_per=options['min_per'], figure_image_list=figure_image_list)
 
@@ -121,7 +107,6 @@ def fit_group_independent(payload, sio, namespace):
 
   hasXlsxReplicates = options['hasXlsxReplicates']
 
-  sio.emit('print', 'fit_group_independent', namespace=namespace)
   df = getDataFrame(fileType, data, hasXlsxReplicates)
 
   figure_image_list = []
@@ -131,19 +116,15 @@ def fit_group_independent(payload, sio, namespace):
   if cosinorType == 'general cosinor':
     n_components = options['n_components']
 
-    sio.emit('print', 'cosinorType == general cosinor', namespace=namespace)
     plot = True
     if (n_components == [1,2,3]):
-      sio.emit('print', 'n_components == [1,2,3]', namespace=namespace)
       plot = False
 
     result_df = cosinor.fit_group(df, n_components = n_components, period=period, plot=plot, figure_image_list=figure_image_list)
 
     if (plot == False):
-      sio.emit('print', 'before get_best_models', namespace=namespace)
       df_best_models = cosinor.get_best_models(df, df_models = result_df, n_components = [1,2,3])
 
-      sio.emit('print', 'before plot_df_models', namespace=namespace)
       cosinor.plot_df_models(df, df_best_models, plot_residuals=False, figure_image_list=figure_image_list)
 
       data = df_best_models.to_csv()
@@ -151,10 +132,7 @@ def fit_group_independent(payload, sio, namespace):
       data = result_df.to_csv()
 
   elif cosinorType == 'cosinor1':
-
-    sio.emit('print', 'cosinorType == cosinor1', namespace=namespace)
     result_df = cosinor1.fit_group(df, period=options['period'], figure_image_list=figure_image_list)
-    sio.emit('print', 'after cosinor1', namespace=namespace)
     data = result_df.to_csv()
 
   return json.dumps({
@@ -172,24 +150,17 @@ def fit_group_population(payload, sio, namespace):
   figure_image_list = []
 
   if cosinorType == 'general cosinor':
-    sio.emit('print', 'cosinorType == general cosinor', namespace=namespace)
 
     n_components = options['n_components']
 
     plot = True
     if (n_components == [1,2,3]):
-      sio.emit('print', 'n_components == [1,2,3]', namespace=namespace)
       plot = False
 
-    sio.emit('print', 'before population_fit_group', namespace=namespace)
     result_df = cosinor.population_fit_group(df, n_components = n_components, period=options['period'], plot_measurements = plot, figure_image_list=figure_image_list)
-    sio.emit('print', 'after population_fit_group', namespace=namespace)
 
     if (plot == False):
       df_best_models = cosinor.get_best_models_population(df, df_models = result_df, n_components = [1,2,3])
-
-      sio.emit('print', 'plot_df_models_population', namespace=namespace)
-      sio.emit('print', figure_image_list, namespace=namespace)
 
       cosinor.plot_df_models_population(df, df_best_models, figure_image_list=figure_image_list)
 
@@ -197,9 +168,7 @@ def fit_group_population(payload, sio, namespace):
     else:
       data = result_df.to_csv()
   elif cosinorType == 'cosinor1':
-    sio.emit('print', 'cosinorType == cosinor1', namespace=namespace)
     result_df = cosinor1.population_fit_group(df, period=options['period'], figure_image_list=figure_image_list)
-    sio.emit('print', 'after cosinor1.population_fit_group', namespace=namespace)
 
     data = result_df.to_csv()
 
@@ -210,27 +179,20 @@ def fit_group_population(payload, sio, namespace):
   })
 
 def comparison_independent(payload, sio, namespace):
-  sio.emit('print', 'comparison_independent', namespace=namespace)
 
   cosinorType, fileType, options, data = validateCosinorAnalysisCommand(payload)
   
   hasXlsxReplicates = options['hasXlsxReplicates']
 
-  sio.emit('print', 'comparison_independent', namespace=namespace)
   df = getDataFrame(fileType, data, hasXlsxReplicates)
-  sio.emit('print', json.dumps(options), namespace=namespace)
 
   figure_image_list = []
 
   period=options['period']
   pairs=options['pairs']
 
-  sio.emit('print', json.dumps(pairs), namespace=namespace)
-  sio.emit('print', 'before if', namespace=namespace)
   if cosinorType == 'general cosinor':
     n_components = options['n_components']
-
-    sio.emit('print', 'cosinorType == general cosinor', namespace=namespace)
 
     df_comp = cosinor.compare_pairs(df, pairs = pairs, n_components = n_components, period=period, figure_image_list=figure_image_list)
 
@@ -247,17 +209,12 @@ def comparison_independent(payload, sio, namespace):
   })
 
 def comparison_population(payload, sio, namespace):
-  sio.emit('print', 'comparison_population validateCosinorAnalysisCommand', namespace=namespace)
 
   cosinorType, fileType, options, data = validateCosinorAnalysisCommand(payload)
   
-  sio.emit('print', fileType, namespace=namespace)
-
   hasXlsxReplicates = options['hasXlsxReplicates']
 
-  sio.emit('print', hasXlsxReplicates, namespace=namespace)
   df = getDataFrame(fileType, data, hasXlsxReplicates)
-  sio.emit('print', json.dumps(options), namespace=namespace)
 
   figure_image_list = []
 
